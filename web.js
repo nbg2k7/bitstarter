8a877244ede4b0c58623e357d6e74a045135f132
NBG <nbg2k7@gmail.com>
2013-07-11 14:19:26 +0000

    Read from index.html -2

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.read('index.html', function(err, data) {
+  fs.readFile('index.html', function(err, data) {
     response.send(data.toString());
-  }
+  });
 });
 
 var port = process.env.PORT || 5000;
